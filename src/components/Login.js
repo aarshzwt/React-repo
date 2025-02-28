b0d9bf9a08240911b45a73c23aee734b47c797da
aarshzwt <aarsh@zealousweb.com>
2025-02-28 12:37:29 +0530

    Added Formik

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,109 +8,160 @@ import axiosInstance from '../utils/axiosInstance';
 import { setLoading, setUserData, setError } from '../redux/slices/authSlice';
 import { useNavigate } from 'react-router-dom';
 import toast from "react-hot-toast";
+import { useFormik } from 'formik';
 
 export default function Login() {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
-
-    const [errors, setErrors] = useState({
-        email: '',
-        password: '',
-    });
-
+    
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    
+    // const [formData, setFormData] = useState({
+    //     email: '',
+    //     password: '',
+    // });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevState) => ({
-            ...prevState,
-            [name]: value,
-        }));
+    // const [errors, setErrors] = useState({
+    //     email: '',
+    //     password: '',
+    // });
+    // const handleChange = (e) => {
+    //     const { name, value } = e.target;
+    //     setFormData((prevState) => ({
+    //         ...prevState,
+    //         [name]: value,
+    //     }));
 
-        // Clear errors on change
-        setErrors((prevState) => ({
-            ...prevState,
-            [name]: '',
-        }));
-    };
+    //     // Clear errors on change
+    //     setErrors((prevState) => ({
+    //         ...prevState,
+    //         [name]: '',
+    //     }));
+    // };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
 
-        // Validation
-        let formIsValid = true;
-        let newErrors = { email: '', password: '' };
-
-        if (!formData.email) {
-            formIsValid = false;
-            newErrors.email = 'Email is required.';
+    const validate = values => {
+        const errors = {};
+        if (!values.email) {
+            errors.email = 'Email is required';
+        }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+          }
+        if (!values.password) { 
+            errors.password = 'Password is required';
         }
+        return errors;
+    }
+    
+    const formik = useFormik({
+        initialValues: {    
+            email: '',
+            password: '',
+        },
+        validate,
+        onSubmit: async (values) => {
+            dispatch(setLoading(true));
 
-        if (!formData.password) {
-            formIsValid = false;
-            newErrors.password = 'Password is required.';
-        }
-
-        if (!formIsValid) {
-            setErrors(newErrors);
-            return;
-        }
-
-        dispatch(setLoading(true));
-
-        try {
-            const response = await axiosInstance.post('auth/login', formData);
-            const { data } = response;
-
-            dispatch(setUserData({
-                user: data.user,
-                role: data.user.role,
-                token: data.token,
-            }));
-
-            if (data.token) {
-                toast.success('User logged in successfully!');
-                navigate("/"); 
-            } else {
-                toast.error('Login failed: Invalid credentials or missing token.');
+            try {
+                const response = await axiosInstance.post('auth/login', values);
+                const { data } = response;
+    
+                dispatch(setUserData({
+                    user: data.user,
+                    role: data.user.role,
+                    token: data.token,
+                }));
+    
+                if (data.token) {
+                    toast.success('User logged in successfully!');
+                    navigate("/"); 
+                } else {
+                    toast.error('Login failed: Invalid credentials or missing token.');
+                }
+            } catch (error) {
+                toast.error('Login failed: Invalid credentials');
+                dispatch(setError(error.response?.data?.message || 'Login failed.'));
             }
-        } catch (error) {
-            toast.error('Login failed: Invalid credentials');
-            dispatch(setError(error.response?.data?.message || 'Login failed.'));
-        }
-    };
+        },
+    });
+
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+
+    //     // Validation
+    //     let formIsValid = true;
+    //     let newErrors = { email: '', password: '' };
+
+    //     if (!formData.email) {
+    //         formIsValid = false;
+    //         newErrors.email = 'Email is required.';
+    //     }
+
+    //     if (!formData.password) {
+    //         formIsValid = false;
+    //         newErrors.password = 'Password is required.';
+    //     }
+
+    //     if (!formIsValid) {
+    //         setErrors(newErrors);
+    //         return;
+    //     }
+
+    //     dispatch(setLoading(true));
+
+    //     try {
+    //         const response = await axiosInstance.post('auth/login', formData);
+    //         const { data } = response;
+
+    //         dispatch(setUserData({
+    //             user: data.user,
+    //             role: data.user.role,
+    //             token: data.token,
+    //         }));
+
+    //         if (data.token) {
+    //             toast.success('User logged in successfully!');
+    //             navigate("/"); 
+    //         } else {
+    //             toast.error('Login failed: Invalid credentials or missing token.');
+    //         }
+    //     } catch (error) {
+    //         toast.error('Login failed: Invalid credentials');
+    //         dispatch(setError(error.response?.data?.message || 'Login failed.'));
+    //     }
+    // };
 
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
             <h1>Login</h1>
-            <Form onSubmit={handleSubmit} className="shadow-lg p-8 rounded-lg bg-white w-[80%] max-w-md">
+            <Form onSubmit={formik.handleSubmit} className="shadow-lg p-8 rounded-lg bg-white w-[80%] max-w-md">
                 <Form.Group controlId="formGridEmail" className="mb-4">
                     <Form.Label>Email *</Form.Label>
                     <Form.Control
                         type="email"
+                        id='email'
                         placeholder="Enter email"
                         name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        isInvalid={!!errors.email} 
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className={formik.touched.email && formik.errors.email ? 'is-invalid' : ''}
                     />
-                    {errors.email && <Form.Control.Feedback type="invalid" className="text-red-500">{errors.email}</Form.Control.Feedback>}
+                    {formik.touched.email && formik.errors.email && <Form.Control.Feedback type="invalid" className="text-red-500">{formik.errors.email}</Form.Control.Feedback>}
                 </Form.Group>
 
                 <Form.Group controlId="formGridPassword" className="mb-4">
                     <Form.Label>Password *</Form.Label>
                     <Form.Control
                         type="password"
+                        id='password'
                         placeholder="Password"
                         name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        isInvalid={!!errors.password}
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className={formik.touched.password && formik.errors.password ? 'is-invalid' : ''}
                     />
-                    {errors.password && <Form.Control.Feedback type="invalid" className="text-red-500">{errors.password}</Form.Control.Feedback>}
+                    {formik.touched.password && formik.errors.password && <Form.Control.Feedback type="invalid" className="text-red-500">{formik.errors.password}</Form.Control.Feedback>}
                 </Form.Group>
 
                 <Form.Group className="flex justify-center">
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,138 +7,203 @@ import axiosInstance from '../utils/axiosInstance';
 import { setLoading, setUserData, setError } from '../redux/slices/authSlice';
 import toast from 'react-hot-toast';
 import { Navigate, useNavigate } from 'react-router-dom';
+import { Formik, useFormik } from 'formik';
 
 export default function Signup() {
-    const [formData, setFormData] = useState({
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-        role: 'customer'
-    });
-
-    const [errors, setErrors] = useState({
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-    });
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevState) => ({
-            ...prevState,
-            [name]: value
-        }));
+    const validate = values => {
+        const errors = {};
+        if (!values.first_name) {
+            errors.first_name = 'First name is required';
+        }
+        if (!values.last_name) {   
+            errors.last_name = 'Last name is required';
+        }
+        if (!values.email) {
+            errors.email = 'Email is required';
+        }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+          }
+        if (!values.password) { 
+            errors.password = 'Password is required';
+        }
+        return errors;
+    }
+    
+    const formik = useFormik({
+        initialValues: {    
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: '',
+            role: 'customer'
+        },
+        validate,
+        onSubmit: async (values) => {
+            dispatch(setLoading(true));
+            try {
+                const response = await axiosInstance.post('auth/register', values);
+                console.log(response);
+                const { data } = response;
+    
+                dispatch(setUserData({
+                    user: data.data,
+                    role: data.data.role,
+                }));
+    
+                if (response.status === 201) {
+                    toast.success('Signed Up Successfully');
+                    navigate("/login");
+                }
+    
+            } catch (error) {
+                console.error('Error:', error);
+                if (error.status === 400) {
+                    toast.error('Email already exists');
+                }
+                dispatch(setError(error.response?.data?.message || 'Signup failed.'));
+            }
+        },
+    });
+
+    // const [formData, setFormData] = useState({
+    //     first_name: '',
+    //     last_name: '',
+    //     email: '',
+    //     password: '',
+    //     role: 'customer'
+    // });
+
+    // const [errors, setErrors] = useState({
+    //     first_name: '',
+    //     last_name: '',
+    //     email: '',
+    //     password: '',
+    // });
+
+    // const handleChange = (e) => {
+    //     const { name, value } = e.target;
+    //     setFormData((prevState) => ({
+    //         ...prevState,
+    //         [name]: value
+    //     }));
 
         // Reset error for the field that is being edited
-        setErrors((prevState) => ({
-            ...prevState,
-            [name]: ''
-        }));
-    };
+    //     setErrors((prevState) => ({
+    //         ...prevState,
+    //         [name]: ''
+    //     }));
+    // };
 
-    const validateForm = () => {
-        const newErrors = {};
+    // const validateForm = () => {
+    //     const newErrors = {};
 
-        // Check for empty fields
-        if (!formData.first_name) newErrors.first_name = 'First name is required';
-        if (!formData.last_name) newErrors.last_name = 'Last name is required';
-        if (!formData.email) newErrors.email = 'Email is required';
-        if (!formData.password) newErrors.password = 'Password is required';
+    //     // Check for empty fields
+    //     if (!formData.first_name) newErrors.first_name = 'First name is required';
+    //     if (!formData.last_name) newErrors.last_name = 'Last name is required';
+    //     if (!formData.email) newErrors.email = 'Email is required';
+    //     if (!formData.password) newErrors.password = 'Password is required';
 
-        setErrors(newErrors);
+    //     setErrors(newErrors);
 
-        return Object.keys(newErrors).length === 0;
-    };
+    //     return Object.keys(newErrors).length === 0;
+    // };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
 
-        if (!validateForm()) return; 
+    //     if (!validateForm()) return; 
 
-        dispatch(setLoading(true));
+    //     dispatch(setLoading(true));
 
-        try {
-            const response = await axiosInstance.post('auth/register', formData);
-            console.log(response);
-            const { data } = response;
+    //     try {
+    //         const response = await axiosInstance.post('auth/register', formData);
+    //         console.log(response);
+    //         const { data } = response;
 
-            dispatch(setUserData({
-                user: data.data,
-                role: data.data.role,
-            }));
+    //         dispatch(setUserData({
+    //             user: data.data,
+    //             role: data.data.role,
+    //         }));
 
-            if (response.status === 201) {
-                toast.success('Signed Up Successfully');
-                navigate("/login");
-            }
+    //         if (response.status === 201) {
+    //             toast.success('Signed Up Successfully');
+    //             navigate("/login");
+    //         }
 
-        } catch (error) {
-            console.error('Error:', error);
-            if (error.status === 400) {
-                toast.error('Email already exists');
-            }
-            dispatch(setError(error.response?.data?.message || 'Signup failed.'));
-        }
-    };
+    //     } catch (error) {
+    //         console.error('Error:', error);
+    //         if (error.status === 400) {
+    //             toast.error('Email already exists');
+    //         }
+    //         dispatch(setError(error.response?.data?.message || 'Signup failed.'));
+    //     }
+    // };
 
     return (
         <div className="flex justify-center items-center h-screen bg-gray-100">
-            <Form onSubmit={handleSubmit} className="shadow-lg p-8 rounded-lg bg-white w-[80%] max-w-md">
+            <Form onSubmit={formik.handleSubmit} className="shadow-lg p-8 rounded-lg bg-white w-[80%] max-w-md">
                 <Form.Group controlId="formGridFirstName" className="mb-4">
                     <Form.Label>First Name *</Form.Label>
                     <Form.Control
                         type="text"
+                        id='first_name'
                         placeholder="First Name"
                         name="first_name"
-                        value={formData.first_name}
-                        onChange={handleChange}
-                        className={errors.first_name ? 'is-invalid' : ''}
+                        value={formik.values.first_name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className={formik.touched.first_name && formik.errors.first_name ? 'is-invalid' : ''}
                     />
-                    {errors.first_name && <div className="text-danger">{errors.first_name}</div>}
+                    {formik.touched.first_name && formik.errors.first_name && <div className="text-danger">{formik.errors.first_name}</div>}
                 </Form.Group>
 
                 <Form.Group controlId="formGridLastName" className="mb-4">
                     <Form.Label>Last Name *</Form.Label>
                     <Form.Control
                         type="text"
+                        id='last_name'
                         placeholder="Last Name"
                         name="last_name"
-                        value={formData.last_name}
-                        onChange={handleChange}
-                        className={errors.last_name ? 'is-invalid' : ''}
+                        value={formik.values.last_name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className={formik.touched.last_name && formik.errors.last_name ? 'is-invalid' : ''}
                     />
-                    {errors.last_name && <div className="text-danger">{errors.last_name}</div>}
+                    {formik.touched.last_name && formik.errors.last_name && <div className="text-danger">{formik.errors.last_name}</div>}
                 </Form.Group>
 
                 <Form.Group controlId="formGridEmail" className="mb-4">
                     <Form.Label>Email *</Form.Label>
                     <Form.Control
                         type="email"
+                        id='email'
                         placeholder="Enter email"
                         name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className={errors.email ? 'is-invalid' : ''}
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className={formik.touched.email && formik.errors.email ? 'is-invalid' : ''}
                     />
-                    {errors.email && <div className="text-danger">{errors.email}</div>}
+                    {formik.touched.email && formik.errors.email && <div className="text-danger">{formik.errors.email}</div>}
                 </Form.Group>
 
                 <Form.Group controlId="formGridPassword" className="mb-4">
                     <Form.Label>Password *</Form.Label>
                     <Form.Control
                         type="password"
+                        id='password'
                         placeholder="Password"
                         name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        className={errors.password ? 'is-invalid' : ''}
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className={formik.touched.password && formik.errors.password ? 'is-invalid' : ''}
                     />
-                    {errors.password && <div className="text-danger">{errors.password}</div>}
+                    {formik.touched.password && formik.errors.password && <div className="text-danger">{formik.errors.password}</div>}
                 </Form.Group>
 
                 <Form.Group className="flex justify-center">
